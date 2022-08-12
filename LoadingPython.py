9f023f0db9f1d87415641b9369e86f1061093cfd
Agven Muharis Rifqi <77918132+agvenmuharisrifqi@users.noreply.github.com>
2022-08-12 17:12:05 +0700

    Update LoadingPython.py

--- LoadingPython.py
+++ LoadingPython.py
@@ -1,9 +1,17 @@
+import time
+
 class Loading:
-    __bar = "█" 
+    __bar  = '█'
     def ProgressBar(self, title, timeout):
+        """
+        Parameters :
+            - title : Title of the progress bar
+            - timeout : Timeout of the progress bar (second)
+        """
         timeout = timeout / 100
         for l in range(101):
-            time.sleep(timeout)
-            print(f"\r{title}\t{self.__bar} {l}%", end="", flush=True)
+            print(f'\r{title} {l}%\t{self.__bar}', flush=True, end="")
             if l % 5 == 0:
-                self.__bar += "█"
+                self.__bar += '█'
+            time.sleep(timeout)
+        print()
